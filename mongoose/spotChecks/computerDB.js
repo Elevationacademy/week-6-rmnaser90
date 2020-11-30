d7c2538e46d1f20842af205f31da2082e809e38d
rmnaser90 <rmnaser90@gmail.com>
2020-11-30 13:14:25 +0200

    computerdb spotcheck

---
+++ mongoose/spotChecks/computerDB.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/computerDB",{useNewUrlParser: true})
+
+
+const Schema = mongoose.Schema
+
+const computerSchema = new Schema({
+    
+})
--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 const mongoose = require("mongoose")
 mongoose.connect("mongodb://localhost/humans",{useNewUrlParser: true})
 
+const Schema = mongoose.Schema
 
-const personSchema = new mongoose.Schema({
+const personSchema = new Schema({
     firstName: String,
     lastName: String,
     age: Number
 })
 
+const courseSchema = new Schema({
+    title: String,
+    duration: Number,
+    sylibus: Array,
+    student: personSchema
+})
+
+const Course = mongoose.model("course", courseSchema)
 const Person = mongoose.model("person", personSchema)
 
 const p1 = new Person({
-    firstName: "khawla",
-    lastName: "Naser",
-    age: 25
+    firstName: "Naya",
+    lastName: "Naser Aldin",
+    age: 9
 })
-p1.save()
 
-Person.find({}, function (error,result) {
-    
-    console.log(result);
+const hiTech = new Course({
+    title: "Hi Tech",
+    duration: "500",
+    sylibus: ["html","css","javscript","jquery","mogodb"],
+    student: p1
+
 })
+
+hiTech.save()
+
+
+
+// Person.find({}, function (error,result) {
+    
+//     console.log(result);
+// })
