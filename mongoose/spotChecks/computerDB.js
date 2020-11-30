77cdbafc18c42ccb1207b0bd6210c5ad864c5ced
rmnaser90 <rmnaser90@gmail.com>
2020-11-30 13:22:56 +0200

    computerDB create Done

--- mongoose/spotChecks/computerDB.js
+++ mongoose/spotChecks/computerDB.js
@@ -1,9 +1,37 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost/computerDB",{useNewUrlParser: true})
+mongoose.connect("mongodb://localhost/computerDB", { useNewUrlParser: true })
 
 
 const Schema = mongoose.Schema
 
 const computerSchema = new Schema({
+    maker: String,
+    price: Number
+})
+
+const Computer = mongoose.model("computer", computerSchema)
+
+
+const c1 = new Computer({
+    maker: "Hp",
+    price: 200
+})
+
+const c2 = new Computer({
+    maker: "apple",
+    price: 1000
+})
+const c3 = new Computer({
+    maker: "Asus",
+    price: 300
+})
+
+const allComputer = [c1, c2, c3]
+
+// allComputer.forEach(c => c.save())
+
+
+Computer.find({maker: "Hp"}, function (err, result) {
+    console.log(result);
     
 })
