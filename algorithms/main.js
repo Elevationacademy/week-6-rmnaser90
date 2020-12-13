ad8d552a42f9c8f4b252122e3594505ffb8e72f5
rmnaser90 <rmnaser90@gmail.com>
2020-12-13 20:31:28 +0200

    finish exercise 2 of algorithms

---
+++ algorithms/main.js
@@ -0,0 +1,35 @@
+// exersice 1
+const printStars = function (num) {
+    
+    for (let i = 0; i < num; i++) {
+        let stars = ""
+        for (let j = 0; j <= i; j++) {
+           
+             stars += "*"
+        }
+        console.log(stars);
+        
+    }
+}
+// printStars(1)
+
+
+// exercise 2
+
+const printBackwardStars = function (num) {
+    
+    for (let i = 0; i < num; i++) {
+        let stars = ""
+        for (let j = num-i; j >0; j--) {
+           
+             stars += "*"
+        }
+        console.log(stars);
+        
+    }
+}
+
+// printBackwardStars(5)
+
+// exercise 3
+
